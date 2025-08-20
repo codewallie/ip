a30c36023a5e6e441b4a5e965857a320c3efba40
codewallie <142010181+codewallie@users.noreply.github.com>
2025-08-20 21:05:30 +0800

    Added add and list feature (stores user input and display when requested).

--- src/main/java/Bro.java
+++ src/main/java/Bro.java
@@ -1,15 +1,24 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Bro {
     private static final String name = "Bro";
     private static final String horizontalLine = "_____________________________________";
-    private static final String exitCmd = "bye";
+    private static ArrayList<String> userInputs = new ArrayList<String>();
 
     public static void main(String[] args) {
         printHello();
         String input = readInput();
-        while (input != null && !input.equals(exitCmd)) {
-            printMsg(input);
+        while (input != null) {
+            if (input.equals("bye")) {
+                break;
+            } else if (input.equals("list")) {
+                printList();
+            } else {
+                userInputs.add(input);
+                printMsg(input);
+            }
+            
             input = readInput();
         }
         printBye();
@@ -24,23 +33,44 @@ public class Bro {
         return input;
     }
 
+    private static void printList() {
+        System.out.println(
+            String.format("\t%s", 
+                horizontalLine));
+        
+        for (int i = 0; i < userInputs.size(); i++) {
+            System.out.println(
+                String.format("\t%d. %s", 
+                    i + 1, userInputs.get(i)));
+        }
+        
+        System.out.println(
+            String.format("\t%s", 
+                horizontalLine));
+    }
+
     private static void printMsg(String msg) {
         System.out.println(String.format(
-                        "\t%s\n\t%s\n\t%s",
-                        horizontalLine, msg, horizontalLine));
+                        "\t%s\n\tadded: %s\n\t%s",
+                        horizontalLine, 
+                        msg, 
+                        horizontalLine));
     }
 
     private static void printHello() {
         System.out.println(
                 String.format(
                         "\t%s\n\tHello! I'm %s\n\tWhat can I do for you?\n\t%s",
-                        horizontalLine, name, horizontalLine));
+                        horizontalLine, 
+                        name, 
+                        horizontalLine));
     }
 
     private static void printBye() {
         System.out.println(
                 String.format(
                         "\t%s\n\tBye. Hope to see you again soon!\n\t%s",
-                        horizontalLine, horizontalLine));
+                        horizontalLine, 
+                        horizontalLine));
     }
 }
