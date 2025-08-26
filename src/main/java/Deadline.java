d50b1b8a2892dd146d0a69b637b952702da00291
codewallie <142010181+codewallie@users.noreply.github.com>
2025-08-26 14:36:43 +0800

    Add LocalDateTime to deadline and event tasks

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,28 +1,33 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    protected String by;
+    protected LocalDateTime by;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, formatter);
     }
 
     public Deadline(String description, boolean isDone, String by) {
         super(description, isDone);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, formatter);
     }
 
     public String toDataString() {
         return String.format("D|%d|%s|%s", 
             (isDone ? 1 : 0), 
             description,
-            by);
+            by.format(formatter));
     }
     
     @Override
     public String toString() {
+        DateTimeFormatter printFormat = DateTimeFormatter.ofPattern("d MMM yyyy hh:mma");
         return String.format("[D] [%s] %s (by: %s)", 
             getStatusIcon(), 
             description,
-            by);
+            by.format(printFormat));
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,33 +1,40 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task {
-    protected String from;
-    protected String to;
+    protected LocalDateTime from;
+    protected LocalDateTime to;
+    private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        this.from = LocalDateTime.parse(from, formatter);
+        this.to = LocalDateTime.parse(to, formatter);
     }
 
     public Event(String description, boolean isDone, String from, String to) {
         super(description, isDone);
-        this.from = from;
-        this.to = to;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("d/M/yyyy HHmm");
+        this.from = LocalDateTime.parse(from, formatter);
+        this.to = LocalDateTime.parse(to, formatter);
     }
 
     public String toDataString() {
         return String.format("E|%d|%s|%s|%s", 
             (isDone ? 1 : 0), 
             description,
-            from,
-            to);
+            from.format(formatter),
+            to.format(formatter));
     }
     
     @Override
     public String toString() {
+        DateTimeFormatter printFormat = DateTimeFormatter.ofPattern("d MMM yyyy hh:mma");
         return String.format("[E] [%s] %s (from: %s to: %s)", 
             getStatusIcon(), 
             description,
-            from,
-            to);
+            from.format(printFormat),
+            to.format(printFormat));
     }
 }
