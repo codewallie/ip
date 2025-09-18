5f6c4fb34a61afe7195e72d3a30727d46f607ac8
codewallie <142010181+codewallie@users.noreply.github.com>
2025-09-18 16:51:15 +0800

    Add title and close window to JavaFX

--- src/main/java/bro/ui/Bro.java
+++ src/main/java/bro/ui/Bro.java
@@ -3,14 +3,19 @@ package bro.ui;
 import java.io.IOException;
 
 import bro.commands.Command;
+import bro.commands.ByeCommand;
 import bro.tasks.Tasks;
 import bro.utils.FileIo;
 import bro.utils.Parser;
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.animation.PauseTransition;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import javafx.util.Duration;
 
 /**
  * The main class for the Bro application, responsible for initializing and
@@ -40,18 +45,27 @@ public class Bro extends Application {
      */
     public String getResponse(String input) {
         Command command = parser.getCommand(input);
-        return command.execute(tasks);
+        String output = command.execute(tasks);
+        if (command instanceof ByeCommand) {
+            PauseTransition delay = new PauseTransition(Duration.seconds(3));
+            delay.setOnFinished(event -> Platform.exit());
+            delay.play();
+        }
+        return output;
     }
 
     @Override
     public void start(Stage stage) {
         try {
+            Image broImage = new Image(this.getClass().getResourceAsStream("/images/broProfilePic.jpg"));
             FXMLLoader fxmlLoader = new FXMLLoader(Bro.class.getResource("/view/MainWindow.fxml"));
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
             stage.setMinHeight(220);
             stage.setMinWidth(417);
+            stage.setTitle("Bro");
+            stage.getIcons().add(broImage);
             fxmlLoader.<MainWindow>getController().setBro(bro);
             stage.show();
         } catch (IOException e) {
